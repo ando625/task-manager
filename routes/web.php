5aa24fa69d9569e3d977220dd9aed65859d27617
ando.yoshie <ando.yoshie1111@gmail.com>
2026-07-22 11:20:54 +0900

    feat: 認証機能の実装（fortify) #3

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
+});
+
+//仮ルート　コントローラ作成時に置き換え
+Route::middleware('auth')->group(function () {
+    Route::get('/tasks', fn() => 'タスク一覧（準備中）')->name('tasks.index');
+    Route::get('/categories', fn() => 'カテゴリー一覧（準備中）')->name('categories.index');
 });
